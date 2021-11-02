6cd92ce082d0a5973beb47577fd2c75ddb8f4e09
AlanisFoz <alanis.foz.pr@gmail.com>
2021-11-02 19:38:35 -0300

    feat: :sparkles: add component ExploreSideBar

---
+++ client/src/components/ExploreSidebar/index.tsx
@@ -0,0 +1,61 @@
+import Button from 'components/Button'
+import Checkbox from 'components/Checkbox'
+import Heading from 'components/Heading'
+import Radio from 'components/Radio'
+
+import * as S from './styles'
+
+export type ItemProps = {
+  title: string
+  name: string
+  type: 'checkbox' | 'radio' | string
+  fields: Field[]
+}
+
+type Field = {
+  label: string
+  name: string
+}
+
+export type ExploreSidebarProps = {
+  items: ItemProps[]
+}
+
+const ExploreSidebar = ({ items }: ExploreSidebarProps) => (
+  <S.Wrapper>
+    {items.map((item) => (
+      <div key={item.name}>
+        <Heading lineBottom lineColor="secondary" size="small">
+          {item.title}
+        </Heading>
+        {item.type === 'checkbox' &&
+          item.fields.map((field) => (
+            <Checkbox
+              key={field.name}
+              name={field.name}
+              label={field.label}
+              labelFor={field.name}
+            />
+          ))}
+
+        {item.type === 'radio' &&
+          item.fields.map((field) => (
+            <Radio
+              key={field.name}
+              id={field.name}
+              value={field.name}
+              name={item.name}
+              label={field.label}
+              labelFor={field.name}
+            />
+          ))}
+      </div>
+    ))}
+
+    <Button fullWidth size="medium">
+      Filter
+    </Button>
+  </S.Wrapper>
+)
+
+export default ExploreSidebar
---
+++ client/src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ExploreSidebar, { ExploreSidebarProps } from '.'
+import items from './mock'
+
+export default {
+  title: 'ExploreSidebar',
+  component: ExploreSidebar,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: {
+    items
+  }
+} as Meta
+
+export const Default: Story<ExploreSidebarProps> = (args) => (
+  <ExploreSidebar {...args} />
+)
